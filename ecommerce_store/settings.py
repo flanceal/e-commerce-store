1e6f41de4453e18d6802715bafffe7c906da7520
danilfartanov <fartanovda@gmail.com>
2023-06-25 16:26:11 +0300

    built models and modified frontend files

--- ecommerce_store/settings.py
+++ ecommerce_store/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'ecommerce_store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecommerce_store',
+        'USER': 'dfartanov',
+        'PASSWORD': 'ecommercestore',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
