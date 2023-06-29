12d8387877111c9904d1207db4aa38a1194866a7
danilfartanov <fartanovda@gmail.com>
2023-06-29 19:25:32 +0300

    implemented log out functionality

--- users/models.py
+++ users/models.py
@@ -5,5 +5,5 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
 
-    image = models.ImageField(upload_to='users/', null=True, blank=True, default='')
+    image = models.ImageField(upload_to='users/', null=True, blank=True)
 
