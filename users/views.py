2e279603dd28d75526489aacb7b7ffc776900a4c
danilfartanov <fartanovda@gmail.com>
2023-06-29 19:25:32 +0300

    implemented log out functionality

--- users/views.py
+++ users/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
 from .models import User
 from .form import UserLoginForm, UserRegistrationForm, UserProfileForm
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 
 
 def login(request):
+    if request.user.is_authenticated:
+        return redirect('users:profile')
     if request.method == 'POST':
         form = UserLoginForm(data=request.POST)
         if form.is_valid():
@@ -27,16 +29,19 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Registration completed successfully')
             return redirect('users:login')
+        else:
+            return render(request, 'users/registration.html', {"form": form})
     form = UserRegistrationForm()
     context = {'form': form}
     return render(request, 'users/registration.html', context)
 
 
-@login_required()
+@login_required(redirect_field_name='login')
 def profile(request):
     if request.method == "POST":
-        form = UserProfileForm(instance=request.user, data=request.POST)
+        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
         if form.is_valid():
             form.save()
             return redirect('users:profile')
@@ -46,3 +51,10 @@ def profile(request):
         form = UserProfileForm(instance=request.user)
     context = {'title': 'Your profile',  "form": form}
     return render(request, 'users/profile.html', context)
+
+
+@login_required(redirect_field_name='login')
+def logout(request):
+    auth.logout(request)
+    return redirect('index')
+
