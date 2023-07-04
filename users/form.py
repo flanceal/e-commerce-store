d27f2cbd52379efeae8b58dcd8b478ec0e0485bb
danilfartanov <fartanovda@gmail.com>
2023-07-04 17:05:24 +0300

    implementing email verification part 1

--- users/form.py
+++ users/form.py
@@ -1,8 +1,12 @@
+import uuid
+from datetime import timedelta
+
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
+from django.utils.timezone import now
 
 
-from users.models import User
+from users.models import User, EmailVerification
 
 
 class UserLoginForm(AuthenticationForm):
@@ -49,6 +53,14 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super(UserRegistrationForm, self).save(commit)
+        expiration = now() + timedelta(hours=48)
+        code = uuid.uuid4()
+        record = EmailVerification.objects.create(code=code, user=user, expiration=expiration)
+        record.send_verification_email()
+        return user
 
 
 class UserProfileForm(UserChangeForm):
--- users/models.py
+++ users/models.py
@@ -1,9 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.mail import send_mail
 
 
 # Create your models here.
 class User(AbstractUser):
-
     image = models.ImageField(upload_to='users/', null=True, blank=True)
+    is_verified_email = models.BooleanField(default=False)
+
+
+class EmailVerification(models.Model):
+    code = models.UUIDField(unique=True)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    expiration = models.DateTimeField()
+
+    def __str__(self):
+        return f"EmailVerification object for {self.user.email}"
+
+    def send_verification_email(self):
+        send_mail(
+            "Subject here",
+            "Here is the message.",
+            "from@example.com",
+            [self.user.email],
+            fail_silently=False,
+        )
+
 
