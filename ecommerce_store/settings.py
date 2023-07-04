a6010953941c3d74a6e63b914e4fbd5a56c3c271
danilfartanov <fartanovda@gmail.com>
2023-07-04 20:46:49 +0300

    finished implementation for verifying user's emails through sending mail

--- ecommerce_store/settings.py
+++ ecommerce_store/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -151,4 +152,12 @@ AUTH_USER_MODEL = 'users.User'
 
 # Sending emails
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'flanceal.store@gmail.com'
+EMAIL_HOST_PASSWORD = 'ejzbxnesynrcemuw'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+
