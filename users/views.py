1e16bcd2bdee1f7c7098167e3c18f508fdac25a8
danilfartanov <fartanovda@gmail.com>
2023-07-04 13:05:55 +0300

    fixed message displaying bugs in reg, log and profile templates

--- users/views.py
+++ users/views.py
@@ -5,24 +5,27 @@ from django.contrib import auth, messages
 from django.contrib.auth.forms import PasswordChangeForm
 from products.models import Basket
 from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.messages.views import SuccessMessageMixin
 
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
 
 
-class UserLoginView(LoginView):
+class UserLoginView(SuccessMessageMixin, LoginView):
     form_class = UserLoginForm
     template_name = 'users/login.html'
+    success_message = f"Welcome back"
 
     def get_success_url(self):
         return reverse_lazy('index')
 
 
-class UserRegistrationView(CreateView):
+class UserRegistrationView(SuccessMessageMixin, CreateView):
     model = User
     form_class = UserRegistrationForm
     template_name = 'users/registration.html'
     success_url = reverse_lazy('users:login')
+    success_message = f"Registration completed successfully"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
@@ -30,10 +33,11 @@ class UserRegistrationView(CreateView):
         return context
 
 
-class UserProfileView(UpdateView):
+class UserProfileView(SuccessMessageMixin, UpdateView):
     model = User
     form_class = UserProfileForm
     template_name = 'users/profile.html'
+    success_message = 'Profile data were edited successfully'
 
     def get_success_url(self):
         return reverse_lazy('users:profile', args=[self.object.id])
@@ -49,8 +53,9 @@ class UserLogoutView(LogoutView):
     next_page = reverse_lazy('index')
 
 
-class UserPasswordChangeView(PasswordChangeView):
+class UserPasswordChangeView(SuccessMessageMixin,PasswordChangeView):
     template_name = 'users/change_password.html'
+    success_message = 'The password has changed successfully'
 
     def get_success_url(self):
         return reverse_lazy('users:profile', args=[self.request.user.id])
