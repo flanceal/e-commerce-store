1af5c57f7921c1365b43d023f55088b8d8b3c488
danilfartanov <fartanovda@gmail.com>
2023-07-12 18:41:50 +0300

    made displaying of each product. created template, view and modified model for it

--- products/admin.py
+++ products/admin.py
@@ -9,7 +9,7 @@ admin.site.register(ProductCategory)
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'price', 'quantity', 'quantity']
-    fields = ['name', 'description', ('price', 'quantity'), 'image', 'category']
+    fields = ['name', 'description', ('price', 'quantity'), 'image', 'category', 'slug']
     readonly_fields = ['description']
     search_fields = ['name']
     ordering = ['name']
