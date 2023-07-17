8a607d1ad3fcb2fc436874a8280acc35165018fa
danilfartanov <fartanovda@gmail.com>
2023-07-17 17:28:39 +0300

    implemented celery and made sending verification email async

---
+++ users/tasks.py
@@ -0,0 +1,16 @@
+from celery import shared_task
+
+from users.models import EmailVerification, User
+
+from django.utils.timezone import now
+import uuid
+from datetime import timedelta
+
+
+@shared_task
+def send_email_verification(user_id):
+    user = User.objects.get(id=user_id)
+    expiration = now() + timedelta(hours=48)
+    code = uuid.uuid4()
+    record = EmailVerification.objects.create(code=code, user=user, expiration=expiration)
+    record.send_verification_email()
